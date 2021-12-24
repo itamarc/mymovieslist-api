0b2fa2cd1b1d1a8e9ae2d668501fb00c7a024e5a
Itamar Carvalho <itamarc@gmail.com>
2021-12-24 15:05:33 -0300

    Initial commit
    Spring Boot Initializr

---
+++ src/main/java/io/itamarc/mymovieslistapi/MymovieslistApiApplication.java
@@ -0,0 +1,13 @@
+package io.itamarc.mymovieslistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymovieslistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymovieslistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/itamarc/mymovieslistapi/MymovieslistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.itamarc.mymovieslistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymovieslistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
