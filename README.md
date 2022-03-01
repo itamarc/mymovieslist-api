d03f83df87a424022b60b68bd85a3ad4c2975599
Itamar Carvalho <itamarc@gmail.com>
2022-03-01 19:43:20 -0300

    Creating README
    And removing an unused import

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Movies List API
+
+[![CircleCI](https://circleci.com/gh/itamarc/mymovieslist-api/tree/master.svg?style=svg)](https://circleci.com/gh/itamarc/mymovieslist-api/tree/master)
+
+This project is the backend API of the **My Movies List** project.
+
+You can see the documentation and details of the project in the [MyMoviesList Github repository](https://github.com/itamarc/mymovieslist/).
--- src/test/java/io/itamarc/mymovieslistapi/repositories/UserRepositoryIT.java
+++ src/test/java/io/itamarc/mymovieslistapi/repositories/UserRepositoryIT.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import io.itamarc.mymovieslistapi.model.AuthProvider;
